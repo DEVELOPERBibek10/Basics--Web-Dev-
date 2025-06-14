3da6cf81736920ec7a17c14300cb8e7e25c6d62c
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2025-06-14 23:57:33 +0545

    add complete Todo app with extra features

--- DOM-Events/Practise/Mini-Projects/Todo-List/script.js
+++ DOM-Events/Practise/Mini-Projects/Todo-List/script.js
@@ -3,9 +3,7 @@ const addBtn = document.querySelector("#add");
 const template = document.querySelector("template");
 let todoList = document.querySelector("#todo-list");
 const filterBtns = document.querySelector(".filters");
-
 let todos = [];
-
 function addTodo(id, text, isCompleted) {
   if (text.trim().length === 0) return;
   const completedFilter = document.querySelector(".Completed");
@@ -24,16 +22,21 @@ function displayTodo(todo) {
   let { id, text } = todo;
   const liFragment = template.content.cloneNode(true);
   const li = liFragment.querySelector("li");
-  li.setAttribute("id", Number(id) + 1);
-  li.querySelector('input[type="checkbox"]').setAttribute("id", Number(id));
-  li.querySelector(".custom-checkbox").setAttribute("for", Number(id));
+  const checkboxId = Math.random();
+  li.setAttribute("id", Number(id));
+  li.querySelector('input[type="checkbox"]').setAttribute("id", checkboxId);
+  li.querySelector(".custom-checkbox").setAttribute("for", checkboxId);
   li.querySelector(".todo-text").textContent = text;
   li.querySelector('input[type="checkbox"]').addEventListener(
     "change",
     function () {
       todo.isCompleted = li.querySelector('input[type="checkbox"]').checked;
+      if (id === Number(li.id)) {
+        li.remove();
+      }
     }
   );
+
   li.querySelector('input[type="checkbox"]').checked = todo.isCompleted;
   todoList.appendChild(li);
 }
@@ -44,16 +47,81 @@ addBtn.addEventListener("click", function () {
 });
 
 todoList.addEventListener("click", function (event) {
-  if (event.target.classList.contains("delete")) {
+  if (event.target.closest(".delete-btn")) {
     const li = event.target.closest("li");
     todos = todos.filter(function (todo) {
-      return todo.id !== Number(li.id - 1);
+      return todo.id !== Number(li.id);
     });
     if (li) {
       li.remove();
     }
   }
-  if (event.target.classList.contains("edit")) {
+  if (event.target.closest(".edit-btn")) {
+    const todo = todos.find(function (todo) {
+      return (
+        todo.isCompleted === false &&
+        todo.id === Number(event.target.closest("li").id)
+      );
+    });
+
+    if (todo) {
+      const li = event.target.closest("li");
+      li.querySelector(".edit").style.display = "none";
+      li.querySelector(".edit").setAttribute("disabled", "");
+      li.querySelector(".delete").style.display = "none";
+      li.querySelector(".delete").setAttribute("disabled", "");
+      li.querySelector(".save").style.display = "inline-block";
+      li.querySelector(".save").removeAttribute("disabled");
+      li.querySelector(".cancel").style.display = "inline-block";
+      li.querySelector(".cancel").removeAttribute("disabled");
+      li.querySelector(".todo-text").style.outline = "auto";
+      li.querySelector(".todo-text").setAttribute("contenteditable", "true");
+    }
+  }
+
+  if (event.target.closest(".save")) {
+    const todo = todos.find(function (todo) {
+      return (
+        todo.isCompleted === false &&
+        todo.id === Number(event.target.closest("li").id)
+      );
+    });
+    if (todo) {
+      const li = event.target.closest("li");
+      if (li.querySelector(".todo-text").textContent.trim().length === 0)
+        return;
+      li.querySelector(".todo-text").setAttribute("contenteditable", "false");
+      li.querySelector(".todo-text").style.outline = "none";
+      li.querySelector(".edit").style.display = "inline-block";
+      li.querySelector(".edit").removeAttribute("disabled");
+      li.querySelector(".delete").style.display = "inline-block";
+      li.querySelector(".delete").removeAttribute("disabled");
+      li.querySelector(".save").style.display = "none";
+      li.querySelector(".save").setAttribute("disabled", "");
+      li.querySelector(".cancel").style.display = "none";
+      li.querySelector(".cancel").setAttribute("disabled", "");
+      todo.text = li.querySelector(".todo-text").textContent;
+    }
+  }
+  if (event.target.closest(".cancel")) {
+    const todo = todos.find(function (todo) {
+      return (
+        todo.isCompleted === false &&
+        todo.id === Number(event.target.closest("li").id)
+      );
+    });
+    if (todo) {
+      const li = event.target.closest("li");
+      li.querySelector(".todo-text").setAttribute("contenteditable", "false");
+      li.querySelector(".todo-text").style.outline = "none";
+      li.querySelector(".edit").style.display = "inline-block";
+      li.querySelector(".delete").style.display = "inline-block";
+      li.querySelector(".save").style.display = "none";
+      li.querySelector(".save").setAttribute("disabled", "");
+      li.querySelector(".cancel").style.display = "none";
+      li.querySelector(".cancel").setAttribute("disabled", "");
+      li.querySelector(".todo-text").textContent = todo.text;
+    }
   }
 });
 
